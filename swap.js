8615617c61c045985ac3f7fbaeb212bcae0aa234
Ramkantodey <ramkantodey@gmail.com>
2024-09-07 16:28:32 +0600

    added done

---
+++ swap.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 7
+let temp = a;
+a = b;
+b = temp;
+console.log(a, b);
+//-----------------------------------
+
+// destructing er maddomew swap kra jay
+let x = 5;
+let y = 7;
+
+[x, y] = [y, x];
+console.log(x, y);
+
+
